2ac883aeaf28f7fcf67411ed135241f95bf189a7
nivoix <jnivoix@yahoo.fr>
2023-08-06 00:50:24 +0200

    ajout infos photographes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -10,8 +10,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3');
+        h3.textContent = data.city+','+""+ data.country;
+        const tag = document.createElement( 'h4');
+        tag.textContent = data.tagline;
+        const price = document.createElement( 'h5');
+        price.textContent = `${data.price}`+'€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(tag);
+        article.appendChild(price);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
